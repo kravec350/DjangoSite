31c1f370ab7587140662933a836fa19c7b458b25
kravec350 <78847694+kravec350@users.noreply.github.com>
2021-02-12 19:02:53 +0200

    добавлено клікабельні ссилки

--- helloworld_project/pages/urls.py
+++ helloworld_project/pages/urls.py
@@ -1,8 +1,9 @@
-from  django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-    path("", views.homePageView, name='home')
+    path("", views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 
 ]
